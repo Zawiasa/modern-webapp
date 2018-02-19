fb0cabd0e7bd68830a2769574e3efc4aa9f71585
Paul Martin Cristian <paul931224@gmail.com>
2018-02-19 22:30:38 +0100

    Figwheel is working, but it doesnt go well with the server-side. Also the server-side is not refreshing on change, it just breaks.

--- project.clj
+++ project.clj
@@ -7,24 +7,90 @@
                  [compojure "1.6.0"]
                  [environ "1.1.0"]
                  [mount "0.1.11"]
+                 [hiccup "1.0.5"]
+                 [com.taoensso/sente "1.11.0"]
                  [ring/ring-defaults "0.3.1"]
                  [ring/ring-jetty-adapter "1.6.2"]
                  [duratom "0.3.7"]
-                 [spootnik/unilog "0.7.22"]]
+
+                 [spootnik/unilog "0.7.22"]
+                 ;;client-side
+                 [re-frame "0.9.4"]
+                 [lein-figwheel "0.5.14"]
+                 [reagent "0.7.0"]
+                 [secretary "1.2.3"]
+                 [jayq "2.5.4"]
+                 [com.google.guava/guava "19.0"]
+                 [org.clojure/core.async    "0.4.474"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [venantius/accountant "0.1.7"
+                  :exclusions [org.clojure/tools.reader]]]
   :ring {:handler webtemp.handlers/app}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :uberjar-name "webtemp.jar"
   :main webtemp.core
   :source-paths ["src"]
-  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [org.clojure/tools.namespace "0.2.11"]
-                                  [pjstadig/humane-test-output "0.8.2"]
-                                  [ring/ring-mock "0.3.2"]]
-                   :plugins [[lein-ring "0.12.2"]
-                             [lein-environ "1.1.0"]]
-                   :env {:config-path "dev/resources/config.yml"}
+  :resource-paths ["resources" "target/cljsbuild"]
+
+  :plugins [[lein-environ "1.0.2"]
+            [lein-cljsbuild "1.1.1"]
+            [lein-asset-minifier "0.2.7"
+             :exclusions [org.clojure/clojure]]]
+  :clean-targets ^{:protect false}
+  [:target-path
+   [:cljsbuild :builds :app :compiler :output-dir]
+   [:cljsbuild :builds :app :compiler :output-to]]
+
+  :cljsbuild
+  {:builds {:app
+              {:source-paths ["src/webtemp-client"]
+               :compiler
+               {:main "webtemp-client.dev"
+                :asset-path "/js/out"
+                :output-to "resources/public/js/app.1.20.js"
+                :output-dir "target/cljsbuild/public/js/out"
+                :source-map true
+                :optimizations :none
+                :pretty-print  true}}
+            :min
+            {
+              :source-paths ["src/webtemp-client"]
+              :compiler
+               {
+                :output-to "resources/public/js/app.1.20.js"
+                :output-dir "target/uberjar"
+                :optimizations :simple
+                :externs ["externs/jquery.js"]
+
+                :pretty-print  false}}}}
+
+
+  :figwheel
+  {:http-server-root "public"
+   :server-port 3449
+   :nrepl-port 7002
+   :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
+
+   :css-dirs ["resources/public/css"]
+   :ring-handler webtemp.handlers/app}
+
+  :profiles {:dev {:repl-options {:init-ns user
+                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   :dependencies [[ring/ring-mock "0.3.0"]
+                                  [ring/ring-devel "1.5.0"]
+                                  [prone "1.1.4"]
+                                  [figwheel-sidecar "0.5.8"]
+                                  [org.clojure/tools.nrepl "0.2.12"]
+                                  [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
+                                  [pjstadig/humane-test-output "0.8.1"]]
+
+                   :plugins [[lein-figwheel "0.5.8"]
+                             [lein-ring "0.12.2"]
+                             [lein-environ "1.1.0"]
+                             [lein-cljsbuild "1.1.1"]]
+                   :env {:config-path "dev/resources/config.yml" :dev true}
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
-                   :repl-options {:init-ns user}
+
                    :source-paths ["dev/src" "test/webtemp"]}})
--- src/webtemp/handlers.clj
+++ src/webtemp/handlers.clj
@@ -2,20 +2,48 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [duratom.core :refer [duratom]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [hiccup.page :refer [include-js include-css html5]]
+            [hiccup.element :refer [javascript-tag]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(def posts 
+(def posts
   (duratom :local-file
            :file-path "db/posts"
            :init [{:id 0 :title "First Post"}]))
 
+
+(defn head []
+  [:head
+   [:meta {:charset "utf-8"}]
+   [:meta {:name "viewport"
+           :content "width=device-width, initial-scale=1"}]
+   [:title "Modern Webapp"]
+   [:link {:rel "icon" :href "/favicon.png"}]
+   (include-css "/css/style.css")
+   (include-css "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css")
+   (include-css "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css")])
+
+
+(defn client-html []
+  (html5
+    (head)
+    [:body {:class "body-container"}
+     [:div#app]
+
+     (include-js "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js")
+     (include-js "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js")
+     (include-js "/js/app.1.20.js")]))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "hmm")
   (GET "/posts" [] (str @posts))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-reload)
+      (wrap-defaults site-defaults)))
 
 (defn get-handler
   []
