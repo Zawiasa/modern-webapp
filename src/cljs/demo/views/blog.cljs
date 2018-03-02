702d267545cee8b2c9fa1a3e238329b75e1b816b
Paul <paul931224@gmail.com>
2018-03-02 12:10:19 +0100

    Blogszerkesztős felület zászlókkal és uk switcherrel

--- src/cljs/demo/views/blog.cljs
+++ src/cljs/demo/views/blog.cljs
@@ -16,25 +16,46 @@
                       :de
                       {:title "de"
                        :content "German"}}})]
+    [:div#my-id
+     {:data-uk-modal "bg-close:	false" :style {:opacity 0.95}}
+     [:div.uk-modal-dialog
+      [:button.uk-modal-close-default
+       {:data-uk-close true :type "button"}]
+      [:div.uk-modal-header [:h2.uk-modal-title "Add new blog entry"]]
+      [:div.uk-modal-body
+       [:div
+        {:data-uk-grid true}
+        [:div.uk-width-auto
+         [:ul.uk-tab-left
+          {:data-uk-tab
+           "connect: #component-tab-left; animation: uk-animation-fade"}
+          (for [language (:languages @input-atom)]
+            (-> ^{:key (first language)}
+             [:li.choose-language
+              [:img.uk-comment-avatar
+               {:alt ""
+                :width "40"
+                :src (str "/img/icons/" (name (first language)) ".svg")}]]))]]
+        [:div.uk-width-expand
+         [:ul#component-tab-left.uk-switcher
+          (for [language (:languages @input-atom)]
+            (-> ^{:key (first language)}
+             [:li
+              [:form.uk-form
+               [:div.uk-margin [:input.uk-input {:placeholder (str (first language) " title")}]]
+               [:div.uk-margin [:textarea.uk-textarea {:placeholder (str (first language) " content")}]]]]))]]]]
 
-    [:div
-     {:data-uk-grid true}
-     [:div.uk-width-auto
-      [:ul.uk-tab-left
-       {:data-uk-tab
-        "connect: #component-tab-left; animation: uk-animation-fade"}
-       (for [language (:languages @input-atom)]
-         (-> ^{:key (first language)}
-          [:li [:a {:href "#"} (first language)]]))]]
-     [:div.uk-width-expand
-      [:ul#component-tab-left.uk-switcher
-       (for [language (:languages @input-atom)]
-         (-> ^{:key (first language)}
-          [:li
-           "Legend"]))]]]))
+      [:div.uk-modal-footer.uk-text-right
+       [:button.uk-button.uk-button-default.uk-modal-close
+        {:type "button"}
+        "Cancel"]
+       [:button.uk-button.uk-button-primary
+        {:type "button"}
+        "Save"]]]]))
 
 (defn blog []
   (let [c (subscribe [:data "shared"])]
     [:div
-     [add-blog]]))
+     [add-blog]
+     [:button.uk-button.uk-button-primary {:data-uk-toggle "target: #my-id"} "Add blog entry"]]))
      ;(str @c)]))
